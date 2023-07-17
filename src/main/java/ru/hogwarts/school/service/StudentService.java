49eef3ef263c1f19c58f27a888df076ea8a064c6
Viacheslav Privalov <privalov2306@yandex.ru>
2023-07-17 18:25:15 +0300

    Init commit

---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,39 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repositoy.StudentRepository;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service
+public class StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student findStudent(long id) {
+
+        return studentRepository.findById(id).get();
+    }
+
+    public Student editStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(long id) {
+
+        studentRepository.deleteById(id);
+    }
+
+    public Collection<Student> getAll() {
+        return studentRepository.findAll();
+    }
+}
